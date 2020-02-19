ab353c452c61ad31e5ed6324541acb10e2029e83
John Xanthopoulos <jxman@hotmail.com>
2020-02-19 16:58:30 -0500

    added location and weather api call

--- tsweather.js
+++ tsweather.js
@@ -1,4 +1,5 @@
 var x = document.getElementById("demo");
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -11,3 +12,23 @@ function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
   "<br>Longitude: " + position.coords.longitude;
 }
+
+$(".btnGetloc").on("click",getLocation);
+
+$(".btnGetWeather").on("click",getWeather);
+
+function getWeather() {
+  $.ajax({
+    url: wurl,
+    type: 'GET',
+    success: function(res) {
+        var text = res.responseBody;
+        console.log ("Weather "+text);
+        // then you can manipulate your text as you wish
+    }
+});  
+}
+
+var wurl="http://api.openweathermap.org/data/2.5/weather?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b"
+
+// api.openweathermap.org/data/2.5/weather?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b
