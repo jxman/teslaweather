0a9f4609a04a16ca9a577cea8961618f43e8b435
John Xanthopoulos <jxman@hotmail.com>
2020-03-09 14:06:58 -0400

    Added symantic ui CSS.  Also added forecast api url in comments for future use.

--- tsweather.js
+++ tsweather.js
@@ -1,34 +1,41 @@
-var x = document.getElementById("demo");
+const x = document.getElementById("demo");
+// const wurl = 'http://api.openweathermap.org/data/2.5/weather?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b';
+
+$(".btnGetloc").on("click",getLocation);
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(getWeather);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+function getWeather(position){
+  const getLat=position.coords.latitude;
+  const getLon=position.coords.longitude;
+
+  const wurl = 'http://api.openweathermap.org/data/2.5/weather?lat='+ getLat + '&lon=' + getLon + '&appid=e61a28138132f74719b96edf26b9c26b';
+  console.log(wurl);
+
+  fetch(wurl)
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson);
+    x.innerHTML = myJson.name +
+    "<br>"+ myJson.weather[0].description +
+    "<br>"+ convertTemp(myJson.main.temp);
+  });
 }
 
-$(".btnGetloc").on("click",getLocation);
+function convertTemp(x){
+  x=((x*1.8)-459.67);
+  return x.toFixed(2);
+};
 
-$(".btnGetWeather").on("click",getWeather);
-
-function getWeather() {
-  $.ajax({
-    url: wurl,
-    type: 'GET',
-    success: function(res) {
-        var text = res.responseBody;
-        console.log ("Weather "+text);
-        // then you can manipulate your text as you wish
-    }
-});  
-}
-
-var wurl="http://api.openweathermap.org/data/2.5/weather?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b"
-
+// Current weather
 // api.openweathermap.org/data/2.5/weather?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b
+// Forecast
+// api.openweathermap.org/data/2.5/forecast?lat=42.205272099999995&lon=-71.23885059999999&appid=e61a28138132f74719b96edf26b9c26b
